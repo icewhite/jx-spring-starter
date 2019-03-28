cd6ecf6facfd840848f64224ccc2293d888980f3
aconway92 <andrewconway92@gmail.com>
2019-03-28 13:34:22 +0000

    Adding a simple test.

--- src/test/java/com/example/jxspringstarter/DemoApplicationTests.java
+++ src/test/java/com/example/jxspringstarter/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.jxspringstarter;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,12 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void simpleTest(){
+		boolean isTrue = true;
+		Assert.assertTrue(isTrue);
 	}
 
 }
